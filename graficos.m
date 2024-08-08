fdea25226dd15ed71f4614f81de58e71787d18d4
Lucas Ungaro <136044528+ungaro398@users.noreply.github.com>
2024-08-08 14:43:54 -0300

    Add files via upload

--- graficos.m
+++ graficos.m
@@ -1,74 +1,163 @@
 clc
 clear
 
-B = 0.001*[75:25:500];
-V = 0.001*[0.008:0.002:0.02,0.021,0.023:0.002:0.033,0.034,0.037,0.039,0.04;...
-    0.017:0.004:0.029,0.032:0.004:0.052,0.055,0.059,0.062,0.067,0.07,0.074,0.078,0.081;...
-    0.026,0.032,0.038,0.043,0.049,0.055,0.06,0.066,0.072,0.077,0.083,0.088,0.094,0.1,0.105,0.111,0.117,0.122;...
-    0.036,0.043,0.051,0.058,0.066,0.074,0.081,0.088,0.096,0.103,0.111,0.118,0.126,0.133,0.141,0.148,0.156,0.163;...
-    -0.02,-0.027,-0.034,-0.042,-0.049,-0.056,-0.064,-0.072,-0.079,-0.086,-0.093,-0.1,-0.108,-0.115,-0.123,-0.13,-0.138,-0.145];
-I = 0.001*[10,20,30,40];
-e = 1.6*10^(-19);
-w = 500*10^(-6);
+import mlreportgen.report.*
+import mlreportgen.dom.*
 
-Vf = [0.0112,0.123];
-If = [10.03,10.8];
+dados1 = importdata('dadosT-4.txt');
+dados2 = importdata('dadosT25.txt');
+dados3 = importdata('dadosT52.txt');
 
-[p1,s1] = polyfit(B,V(1,:),1);
-[p2,s2] = polyfit(B,V(2,:),1);
-[p3,s3] = polyfit(B,V(3,:),1);
-[p4,s4] = polyfit(B,V(4,:),1);
-[p5,s5] = polyfit(B,V(5,:),1);
+T = [-4, 25, 52];
+T = T + 273.15;
 
-[Y1,delta1] = polyval(p1,B,s1);
-[Y2,delta2] = polyval(p2,B,s2);
-[Y3,delta3] = polyval(p3,B,s3);
-[Y4,delta4] = polyval(p4,B,s4);
-[Y5,delta5] = polyval(p5,B,s5);
+y1 = log(dados1.data(:,2));
+y2 = log(dados2.data(:,2));
+y3 = log(dados3.data(:,2));
 
-n = [I(1)./(p1(1)*e*w),I(2)./(p2(1)*e*w),I(3)./(p3(1)*e*w),I(4)./(p4(1)*e*w),I(4)./(p5(1)*e*w)];
-nf = mean(n(1:4));
+x1 = dados1.data(:,1);
+x2 = dados2.data(:,1);
+x3 = dados3.data(:,1);
 
-un = [];
-un(1) = n(1)*sqrt((1/w)^2 + (1/e)^2 + (1/p1(1))^2 + (s1.normr)^2 + (1/I(1))^2);
-un(2) = n(2)*sqrt((1/w)^2 + (1/e)^2 + (1/p2(1))^2 + (s2.normr)^2 + (1/I(2))^2);
-un(3) = n(3)*sqrt((1/w)^2 + (1/e)^2 + (1/p3(1))^2 + (s3.normr)^2 + (1/I(3))^2);
-un(4) = n(4)*sqrt((1/w)^2 + (1/e)^2 + (1/p4(1))^2 + (s4.normr)^2 + (1/I(4))^2);
-un(5) = n(5)*sqrt((1/w)^2 + (1/e)^2 + (1/p5(1))^2 + (s5.normr)^2 + (1/I(4))^2);
+erry1 = 0.01*ones(size(y1));
+erry2 = 0.01*ones(size(y2));
+erry3 = 0.01*ones(size(y3));
 
-RH = 1./(e*n(1:4));
-uRH = RH.*sqrt((1/e)^2 + (1./n(1:4)).^2 + un(1:4).^2);
+errx1 = 0.0001*ones(size(x1));
+errx2 = 0.0001*ones(size(x2));
+errx3 = 0.0001*ones(size(x3));
 
-errB = 0.001*ones(size(B));
-errV = 0.000001*ones(size(V(1,:)));
+[p1,s1] = polyfit(x1,y1,1);
+[p2,s2] = polyfit(x2,y2,1);
+[p3,s3] = polyfit(x3,y3,1);
+
+ek1 = p1(1)*(T(1));
+ek2 = p2(1)*(T(2));
+ek3 = p3(1)*(T(3));
+
+uek1 = ek1*sqrt((1/T(1))^2 + (s1.normr/p1(1))^2);
+uek2 = ek2*sqrt((1/T(2))^2 + (s2.normr/p2(1))^2);
+uek3 = ek3*sqrt((1/T(3))^2 + (s3.normr/p3(1))^2);
+
+mediaek = (ek1+ek2+ek3)/3;
+mediauek = sqrt(uek1^2+uek2^2+uek3^3);
+
+k1 = (1.6e-19)/ek1;
+k2 = (1.6e-19)/ek2;
+k3 = (1.6e-19)/ek3;
+
+uk1 = k1*sqrt((1/T(1))^2 + (s1.normr/p1(1))^2);
+uk2 = k2*sqrt((1/T(1))^2 + (s1.normr/p1(1))^2);
+uk3 = k3*sqrt((1/T(1))^2 + (s1.normr/p1(1))^2);
+
+mediak = (k1+k2+k3)/3;
+mediauk = sqrt(uk1^2+uk2^2+uk3^3);
 
 figure(1);
-errorbar(B,V(1,:),errV,errV,errB,errB,'.r','MarkerSize',8);
+[Y1,delta1] = polyval(p1,x1,s1);
+errorbar(x1,y1,errx1,errx1,erry1,erry1,'.','MarkerSize',8);
 hold on;
-errorbar(B,V(2,:),errV,errV,errB,errB,'.g','MarkerSize',8);
-errorbar(B,V(3,:),errV,errV,errB,errB,'.b','MarkerSize',8);
-errorbar(B,V(4,:),errV,errV,errB,errB,'.c','MarkerSize',8);
-plot(B,Y1,'red',B,Y2,'green',B,Y3,'blue',B,Y4,'cyan');
-xlabel('Campo Magnético [mT]');
-ylabel('Tensão Hall [V]');
-legend('10 mA','20 mA','30 mA','40 mA');
+plot(x1,Y1);
+title('Determinação de e/k em -4ºC');
+xlabel('V_{eb} (V)');
+ylabel('ln(I_c) (\mu A)');
+legend('Pontos','Ajuste; $\alpha=$41,92$\pm$0,25','Interpreter','latex','Location','northwest');
 grid on;
 hold off;
-saveas(figure(1),'graficoHall.png');
+saveas(figure(1),'graficoT-4.png');
 
 figure(2);
-errorbar(B,V(5,:),errV,errV,errB,errB,'.r','MarkerSize',8);
+[Y2,delta2] = polyval(p2,x2,s2);
+errorbar(x2,y2,errx2,errx2,erry2,erry2,'.','MarkerSize',8);
 hold on;
-plot(B,Y5,'red');
-xlabel('Campo Magnético [mT]');
-ylabel('Tensão Hall [V]');
-legend('40 mA');
+plot(x2,Y2);
+title('Determinação de e/k em 25ºC');
+xlabel('V_{eb} (V)');
+ylabel('ln(I_c) (\mu A)');
+legend('Pontos','Ajuste; $\alpha=$38,41$\pm$0,33','Interpreter','latex','Location','northwest');
 grid on;
 hold off;
-saveas(figure(2),'graficoHall_180.png');
+saveas(figure(2),'graficoT25.png');
 
-R1 = Vf(1)/If(1);
-R2 = Vf(2)/If(2);
+figure(3);
+[Y3,delta3] = polyval(p3,x3,s3);
+errorbar(x3,y3,errx3,errx3,erry3,erry3,'.','MarkerSize',8);
+hold on;
+plot(x3,Y3);
+title('Determinação de e/k em 52ºC');
+xlabel('V_{eb} (V)');
+ylabel('ln(I_c) (\mu A)');
+legend('Pontos','Ajuste; $\alpha=$30,37$\pm$0,25','Interpreter','latex','Location','northwest');
+grid on;
+hold off;
+saveas(figure(3),'graficoT52.png');
 
-ro = (pi*w*(R1+R2))/(log(2)*2);
-mu = 1/(ro*e*nf);
+rpt = Report('Relatório dos graficos de ek','pdf');
+
+tp = TitlePage();
+tp.Title = 'Relatório doa Gráficos de ek';
+tp.Author = 'Lucas Almeida de Ungaro';
+tp.PubDate = date();
+
+add(rpt,tp);
+
+append(rpt,TableOfContents);
+
+ch = Chapter('Ajustes com Distribuição de Pontos em Reta');
+
+imagePath = which('graficoT-4.png');
+img1 = Image(imagePath);
+img1.Style = [img1.Style {ScaleToFit}];
+append(ch,Section('Title','Gráfico para T-4','Content',img1));
+append(ch,"e/k(1) = ");
+append(ch,Section('Content',ek1));
+append(ch,"u(e/k(1)) = ");
+append(ch,Section('Content',uek1));
+append(ch,"k(1) = ");
+append(ch,Section('Content',k1));
+append(ch,"uk(1) = ");
+append(ch,Section('Content',uk1));
+
+imagePath = which('graficoT25.png');
+img2 = Image(imagePath);
+img2.Style = [img2.Style {ScaleToFit}];
+append(ch,Section('Title','Gráfico para T25','Content',img2));
+append(ch,"e/k(2) = ");
+append(ch,Section('Content',ek2));
+append(ch,"u(e/k(2)) = ");
+append(ch,Section('Content',uek2));
+append(ch,"k(2) = ");
+append(ch,Section('Content',k2));
+append(ch,"uk(2) = ");
+append(ch,Section('Content',uk2));
+
+imagePath = which('graficoT52.png');
+img3 = Image(imagePath);
+img3.Style = [img3.Style {ScaleToFit}];
+append(ch,Section('Title','Gráfico para T52','Content',img3));
+append(ch,"e/k(3) = ");
+append(ch,Section('Content',ek3));
+append(ch,"u(e/k(3)) = ");
+append(ch,Section('Content',uek3));
+append(ch,"k(3) = ");
+append(ch,Section('Content',k3));
+append(ch,"uk(3) = ");
+append(ch,Section('Content',uk3));
+
+append(rpt,ch);
+
+ch2 = Chapter('Médias');
+
+append(ch2,"e/k = ");
+append(ch2,Section('Content',mediaek));
+append(ch2,"u(e/k) = ");
+append(ch2,Section('Content',mediauek));
+append(ch2,"k = ");
+append(ch2,Section('Content',mediak));
+append(ch2,"u(k) = ");
+append(ch2,Section('Content',mediauk));
+
+append(rpt,ch2);
+
+close(rpt);
+rptview(rpt);
